691ca096994b8764c2d91f494728c29e804de175
Brett Tanonaka <tano.brett@gmail.com>
2023-07-27 15:37:05 -0700

    Initialized webpack.config file with HtmlWebpackPlugin

---
+++ webpack.config.ts
@@ -0,0 +1,31 @@
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+export default {
+  mode: 'development',
+  entry: path.join(__dirname, './client/src/index.tsx'),
+  output: {
+    path: path.join(__dirname, '/client/dist'),
+    filename: 'bundle.js',
+  },
+  cache: false,
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx?)$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, '/client/dist/index.html'),
+      inject: false,
+    }),
+  ],
+};
