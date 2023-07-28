2b5c6ea4eb7812680db10e4a9959fc4e21cbc3e5
Brett Tanonaka <tano.brett@gmail.com>
2023-07-28 15:53:04 -0700

    Created interfaces file, changed names of functions in controller, changed postValues in controller to map through data

--- server/controller.ts
+++ server/controller.ts
@@ -1,20 +1,22 @@
 import { RequestHandler } from 'express';
-import { getAllNumbers, getOneFromN, saveOneValue } from './model';
+import { DataRow } from '../interfaces';
+import { getMaxValue, getOneFromN, saveOneValue } from './model';
 
-export const getAll: RequestHandler = (req, res) => {
-  getAllNumbers()
+export const getMaxN: RequestHandler = (req, res) => {
+  getMaxValue()
     .then((data) => { res.status(200).send(data); })
     .catch((err: unknown) => { res.status(502).send(err); });
 };
 
 export const getValueAtN: RequestHandler = (req, res) => {
-  getOneFromN(req.params.number)
+  getOneFromN(req.query.start as string, req.query.end as string)
     .then((data) => res.status(200).send(data))
     .catch((err: unknown) => res.status(502).send(err));
 };
 
-export const postOneValue: RequestHandler = (req, res) => {
-  saveOneValue(req.body)
-    .then(() => { res.status(201).send(); })
+export const postValues: RequestHandler = (req, res) => {
+  const values = req.body.map((row: DataRow) => [row.n, row.current, row.previous]);
+  saveOneValue(values)
+    .then(() => { res.status(201).send('Input added sucessfully'); })
     .catch((err: unknown) => { res.status(422).send(err); });
 };
