23c93c802b8f8710730e4370833743b12aa8a03c
Brett Tanonaka <tano.brett@gmail.com>
2023-07-28 17:04:26 -0700

    NumberInput only takes in numbers and form is able to submit, not connected to db

--- client/src/components/NumberInput.tsx
+++ client/src/components/NumberInput.tsx
@@ -1,12 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, FormEvent } from 'react';
 
 export default function NumberInput() {
-  const [fibN, setFibN] = useState<string>('');
+  const [fibN, setFibN] = useState<number | string>('');
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <input
-        type="text"
-        name="number"
+        type="number"
+        pattern="[0-9]*"
+        inputMode="numeric"
         value={fibN}
         placeholder="Enter a number"
         onChange={(e) => setFibN(e.target.value)}
